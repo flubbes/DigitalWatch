2cb41384c17038aa1cb999ab304189baedde934d
Fabian Berg <info@fabian-berg.de>
2015-02-16 23:06:34 +0100

    Added ThreadProxy functionality. IsBackground not yet implemented (probably not needed)

---
+++ DigitalWatch/DigitalWatch/Ticks/ThreadProxy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading;
+
+namespace DigitalWatch.Ticks
+{
+    public class ThreadProxy : IThreadProxy
+    {
+        public void Run(Action action)
+        {
+            new Thread(action.Invoke).Start();
+        }
+    }
+}
