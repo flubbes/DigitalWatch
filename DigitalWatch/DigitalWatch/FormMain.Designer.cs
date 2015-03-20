6aec1549048de0835f47648ef347954b68f25682
Fabian Berg <info@fabian-berg.de>
2015-03-20 02:12:03 +0100

    added light and set button

--- DigitalWatch/DigitalWatch/FormMain.Designer.cs
+++ DigitalWatch/DigitalWatch/FormMain.Designer.cs
@@ -30,6 +30,8 @@
         {
             this.sevenSegmentArray = new DigitalWatch.Controls.SevenSegmentArray();
             this.btnMode = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.btnLight = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // sevenSegmentArray
@@ -52,20 +54,44 @@
             // btnMode
             // 
             this.btnMode.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btnMode.Location = new System.Drawing.Point(427, 2);
+            this.btnMode.Location = new System.Drawing.Point(427, 12);
             this.btnMode.Name = "btnMode";
             this.btnMode.Size = new System.Drawing.Size(75, 23);
             this.btnMode.TabIndex = 1;
             this.btnMode.Text = "Mode";
             this.btnMode.UseVisualStyleBackColor = true;
             // 
+            // btnSet
+            // 
+            this.btnSet.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSet.Location = new System.Drawing.Point(427, 41);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(75, 23);
+            this.btnSet.TabIndex = 2;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = true;
+            // 
+            // btnLight
+            // 
+            this.btnLight.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnLight.Location = new System.Drawing.Point(427, 70);
+            this.btnLight.Name = "btnLight";
+            this.btnLight.Size = new System.Drawing.Size(75, 23);
+            this.btnLight.TabIndex = 3;
+            this.btnLight.Text = "Light";
+            this.btnLight.UseVisualStyleBackColor = true;
+            // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(562, 237);
+            this.ClientSize = new System.Drawing.Size(514, 134);
+            this.Controls.Add(this.btnLight);
+            this.Controls.Add(this.btnSet);
             this.Controls.Add(this.btnMode);
             this.Controls.Add(this.sevenSegmentArray);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
             this.Name = "FormMain";
             this.Text = "DigitalWatch";
             this.ResumeLayout(false);
@@ -76,6 +102,8 @@
 
         private Controls.SevenSegmentArray sevenSegmentArray;
         private System.Windows.Forms.Button btnMode;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Button btnLight;
     }
 }
 
