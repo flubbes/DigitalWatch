8f89be4e97993cb77c5ce56902f1dd856073b3ec
Fabian Berg <info@fabian-berg.de>
2015-03-20 16:46:13 +0100

    added factory docs and functionality to instantiate default tick

--- DigitalWatch/DigitalWatch/DefaultClockFactory.cs
+++ DigitalWatch/DigitalWatch/DefaultClockFactory.cs
@@ -1,19 +1,29 @@
 ﻿using DigitalWatch.Behaviors;
 using DigitalWatch.Core;
 using DigitalWatch.Displays;
+using DigitalWatch.Ticks;
 
 namespace DigitalWatch
 {
+    /// <summary>
+    /// A factory to create a default clock and all of its properties
+    /// </summary>
     public class DefaultClockFactory
     {
+        /// <summary>
+        /// Creates a new default clock with all of its properties
+        /// </summary>
+        /// <returns>The built clock</returns>
         public Clock Create()
         {
             var defaultBehavior = new TimeBehavior();
             var defaultDisplay = new DefaultDisplay();
+
             var defaultClock = new Clock
             {
                 Behavior = defaultBehavior,
-                Display = defaultDisplay
+                Display = defaultDisplay,
+                TickControl = new DefaultClockTickControl()
             };
             defaultBehavior.SetClock(defaultClock);
             return defaultClock;
