2374fe99ca65acb22e89df34923d73e1f4891f4f
Fabian Berg <info@fabian-berg.de>
2015-03-20 02:04:27 +0100

    renamed the sevenSegmentArray

--- DigitalWatch/DigitalWatch/FormMain.Designer.cs
+++ DigitalWatch/DigitalWatch/FormMain.Designer.cs
@@ -28,33 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.sevenSegmentArray1 = new DigitalWatch.Controls.SevenSegmentArray();
+            this.sevenSegmentArray = new DigitalWatch.Controls.SevenSegmentArray();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // sevenSegmentArray1
+            // sevenSegmentArray
             // 
-            this.sevenSegmentArray1.ArrayCount = 4;
-            this.sevenSegmentArray1.ColorBackground = System.Drawing.Color.DarkGray;
-            this.sevenSegmentArray1.ColorDark = System.Drawing.Color.DimGray;
-            this.sevenSegmentArray1.ColorLight = System.Drawing.Color.Red;
-            this.sevenSegmentArray1.DecimalShow = true;
-            this.sevenSegmentArray1.ElementPadding = new System.Windows.Forms.Padding(4);
-            this.sevenSegmentArray1.ElementWidth = 10;
-            this.sevenSegmentArray1.ItalicFactor = 0F;
-            this.sevenSegmentArray1.Location = new System.Drawing.Point(12, 2);
-            this.sevenSegmentArray1.Name = "sevenSegmentArray1";
-            this.sevenSegmentArray1.Size = new System.Drawing.Size(451, 149);
-            this.sevenSegmentArray1.TabIndex = 0;
-            this.sevenSegmentArray1.TabStop = false;
-            this.sevenSegmentArray1.Value = null;
+            this.sevenSegmentArray.ArrayCount = 4;
+            this.sevenSegmentArray.ColorBackground = System.Drawing.Color.DarkGray;
+            this.sevenSegmentArray.ColorDark = System.Drawing.Color.DimGray;
+            this.sevenSegmentArray.ColorLight = System.Drawing.Color.Red;
+            this.sevenSegmentArray.DecimalShow = true;
+            this.sevenSegmentArray.ElementPadding = new System.Windows.Forms.Padding(4);
+            this.sevenSegmentArray.ElementWidth = 10;
+            this.sevenSegmentArray.ItalicFactor = 0F;
+            this.sevenSegmentArray.Location = new System.Drawing.Point(12, 2);
+            this.sevenSegmentArray.Name = "sevenSegmentArray";
+            this.sevenSegmentArray.Size = new System.Drawing.Size(409, 131);
+            this.sevenSegmentArray.TabIndex = 0;
+            this.sevenSegmentArray.TabStop = false;
+            this.sevenSegmentArray.Value = null;
             // 
-            // Form1
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(427, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(562, 237);
-            this.Controls.Add(this.sevenSegmentArray1);
-            this.Name = "Form1";
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sevenSegmentArray);
+            this.Name = "FormMain";
             this.Text = "DigitalWatch";
             this.ResumeLayout(false);
 
@@ -62,7 +73,8 @@
 
         #endregion
 
-        private Controls.SevenSegmentArray sevenSegmentArray1;
+        private Controls.SevenSegmentArray sevenSegmentArray;
+        private System.Windows.Forms.Button button1;
     }
 }
 
