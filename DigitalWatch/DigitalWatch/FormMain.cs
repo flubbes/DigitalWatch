4f5b5ef778c487dc6bf8c92576302c2d4992c1e9
Fabian Berg <info@fabian-berg.de>
2015-03-20 15:47:06 +0100

    updated documentation of mainForm

--- DigitalWatch/DigitalWatch/FormMain.cs
+++ DigitalWatch/DigitalWatch/FormMain.cs
@@ -5,14 +5,32 @@ using System.Windows.Forms;
 
 namespace DigitalWatch
 {
+    /// <summary>
+    /// A Form with all the possibilities
+    ///  of displaying a DigitalWatch
+    /// </summary>
     public partial class FormMain : Form
     {
         private readonly IClock _clock;
 
+        /// <summary>
+        /// Initializes a new instance of FormMain
+        /// </summary>
         public FormMain()
         {
             InitializeComponent();
             _clock = new DefaultClockFactory().Create();
+            _clock.Display.Update += Display_Update;
+        }
+
+        /// <summary>
+        /// Gets triggered when the display updates
+        /// </summary>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">The argument data container of the event</param>
+        private void Display_Update(object sender, Displays.UpdateEvent.UpdateEventArgs e)
+        {
+            sevenSegmentArray.Value = e.DisplayData;
         }
 
         /// <summary>
@@ -29,17 +47,17 @@ namespace DigitalWatch
         /// Is executed when the set button is clicked
         /// </summary>
         /// <param name="sender">The sender of the event</param>
-        /// <param name="e">The </param>
+        /// <param name="e">The argument container transmitted with the event</param>
         private void btnSet_Click(object sender, EventArgs e)
         {
             _clock.RegisterClick(new SetClick());
         }
 
         /// <summary>
-        ///
+        /// Is executed when the light button is clicked
         /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
+        /// <param name="sender">The sender of the event</param>
+        /// <param name="e">The argument container transmitted with the event</param>
         private void btnLight_Click(object sender, EventArgs e)
         {
             _clock.RegisterClick(new LightClick());
