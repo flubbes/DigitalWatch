a5d0c84e6ea2c3e07b6f92569829e16ed71223f1
Marvin Karaschewski <marvin@kabesoft.de>
2015-03-20 14:01:13 +0100

    implemented some basic properties of the clock, as well as the DefaultClockFactory

--- DigitalWatch/DigitalWatch.Tests/DefaultClockFactoryTests.cs
+++ DigitalWatch/DigitalWatch.Tests/DefaultClockFactoryTests.cs
@@ -1,14 +1,30 @@
-﻿using NUnit.Framework;
+﻿using System.Collections.Generic;
+using DigitalWatch.Behaviors;
+using DigitalWatch.Core;
+using DigitalWatch.Displays;
+using FluentAssertions;
+using NUnit.Framework;
 
 namespace DigitalWatch.Tests
 {
     [TestFixture]
     public class DefaultClockFactoryTests
     {
+        private DefaultClockFactory _factory;
+
         [Test]
         public void CanCreateFactory()
         {
-            var factory = new DefaultClockFactory();
+            _factory = new DefaultClockFactory();
+        }
+
+        [Test]
+        public void ClockPropertiesHaveConcreteTypes()
+        {
+            var clock = _factory.CreateDefaultClock();
+            clock.Behavior.Should().BeOfType<TimeBehavior>();
+            clock.Display.Should().BeOfType<DefaultDisplay>();
+            clock.Buttons.Should().BeOfType<List<IClockButton>>();
         }
     }
 }
--- DigitalWatch/DigitalWatch/DefaultClockFactory.cs
+++ DigitalWatch/DigitalWatch/DefaultClockFactory.cs
@@ -1,7 +1,24 @@
-﻿namespace DigitalWatch
+﻿using System.Collections.Generic;
+using DigitalWatch.Behaviors;
+using DigitalWatch.Core;
+using DigitalWatch.Displays;
+
+namespace DigitalWatch
 {
     public class DefaultClockFactory
     {
-         
+        public Clock CreateDefaultClock()
+        {
+            var defaultBehavior = new TimeBehavior();
+            var defaultDisplay = new DefaultDisplay();
+            var defaultButtons = new List<IClockButton>();
+            var defaultClock = new Clock()
+            {
+                Behavior = defaultBehavior,
+                Display = defaultDisplay,
+                Buttons = defaultButtons
+            };
+            return defaultClock;
+        }
     }
 }
