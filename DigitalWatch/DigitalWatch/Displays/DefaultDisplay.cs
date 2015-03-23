19656008bbd4cbc449aa3684910a4f8d774f2a5c
Marvin Karaschewski <marvin@kabesoft.de>
2015-03-23 12:08:56 +0100

    Added documentation

--- DigitalWatch/DigitalWatch/Displays/DefaultDisplay.cs
+++ DigitalWatch/DigitalWatch/Displays/DefaultDisplay.cs
@@ -4,6 +4,10 @@ namespace DigitalWatch.Displays
 {
     public class DefaultDisplay : IClockDisplay
     {
+        /// <summary>
+        /// The Update-Routine for the display
+        /// </summary>
+        /// <param name="displayData"></param>
         public void TriggerUpdate(string displayData)
         {
             if (Update != null)
--- DigitalWatch/DigitalWatch/Displays/IClockDisplay.cs
+++ DigitalWatch/DigitalWatch/Displays/IClockDisplay.cs
@@ -2,6 +2,9 @@
 
 namespace DigitalWatch.Displays
 {
+    /// <summary>
+    /// The interface from which all the displays must be derived
+    /// </summary>
     public interface IClockDisplay
     {
         event UpdateEventHandler Update;
